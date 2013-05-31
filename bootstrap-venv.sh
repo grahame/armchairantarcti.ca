c1605cee496a89fe48d7bdabc26253dc57183482
grahame@angrygoats.net <grahame@angrygoats.net>
2013-05-31 19:33:00 +0800

    bootstrap

--- bootstrap-venv.sh
+++ bootstrap-venv.sh
@@ -1,4 +1,13 @@
+#
+# set up virtualenv
+#
 VENV=~/venv.arm # hgfs sucks
 rm -rf $VENV
 virtualenv --system-site-packages -p /usr/bin/python $VENV
 $VENV/bin/easy_install -U psycopg2 simplejson markdown flask flask-sqlalchemy openpyxl
+#
+# set up openlayers
+#
+rsync -az --delete ./openlayers/theme/ ./html/theme/
+rsync -az --delete ./openlayers/img/ ./html/img/
+( cd openlayers/build && python build.py -c jsmin full ../../html/OpenLayers.js )
