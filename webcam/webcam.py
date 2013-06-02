e26828b5b7594932a7b119dc9c219e7c0503baa0
Grahame Bowland <grahame@angrygoats.net>
2013-06-02 01:05:08 +0000

    a bunch more webcams

--- webcam/webcam.py
+++ webcam/webcam.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import requests, lxml, lxml.etree, sys, os, Image, json
+import urlparse
 import traceback
 from StringIO import StringIO
 
@@ -13,6 +14,11 @@ class Cam(object):
         self.uri = uri
         self.descr = descr
 
+    def get_html(self, uri):
+        req = requests.get(uri)
+        f = StringIO(req.content)
+        return lxml.etree.parse(f, html)
+
     def thumb_filename(self):
         return "tn_%s.jpg" % self.name
 
@@ -20,6 +26,7 @@ class Cam(object):
         return os.path.join("../html/webcam/", self.thumb_filename())
 
     def make_thumb(self, img_uri):
+        img_uri = urlparse.urljoin(self.uri, img_uri)
         print "thumbnail:", img_uri
         req = requests.get(img_uri)
         outf = self.thumb_path()
@@ -40,23 +47,27 @@ class Cam(object):
 
 class AAD(Cam):
     def update(self):
-        req = requests.get(self.uri)
-        f = StringIO(req.content)
-        et = lxml.etree.parse(f, html)
+        et = self.get_html(self.uri)
         img = et.xpath('//div[@id="article"]/div/div/img')[0]
         self.make_thumb(img.get('src'))
 
 class Kiwi(Cam):
     def update(self):
-        req = requests.get(self.uri)
-        f = StringIO(req.content)
-        et = lxml.etree.parse(f, html)
-        img = et.xpath('//div[@id="article"]/div/div/img')[0]
+        et = self.get_html(self.uri)
+        img = et.xpath('//img[contains(@src, "/webcam/")]')[0]
         self.make_thumb(img.get('src'))
 
+class Thistle(Cam):
+    def update(self):
+        et = self.get_html(self.uri)
+        img = et.xpath('//center/img')[0]
+        self.make_thumb(img.get('src'))
 
 if __name__ == '__main__':
     webcams = [
+            ( Thistle, ("union", "http://www.thistle.org/cgi-bin/dispcgi_wx7_latest_03?today.csv", "Union Glacier runway")),
+            ( Thistle, ("thiel", "http://www.thistle.org/cgi-bin/dispcgi_wx8_latest_01?today.csv", "Thiel Mountains runway")),
+            ( Kiwi, ("scott", "http://www.antarcticanz.govt.nz/scott-base/webcams", "Scott base")),
             ( AAD, ("casey", "http://www.antarctica.gov.au/webcams/casey", "Casey station") ),
             ( AAD, ("davis", "http://www.antarctica.gov.au/webcams/davis", "Davis station") ),
             ( AAD, ("macquarie-island", "http://www.antarctica.gov.au/webcams/macquarie-island", "Macquarie Island station") ),
